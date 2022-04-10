321f4e7548f7904781fc6e16bc94fc0f52d900d8
lan-mak <lan-mak@ya.ru>
2022-04-10 23:12:20 +0300

    temp10042022

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -5,90 +5,48 @@
 const createRequest = (options = {}) => {
 
   const xhr = new XMLHttpRequest;
-  const formData = new FormData;
+  let formData = new FormData;
+
+  let urlSource = options.url;
+  let data = options.data;
+
+  if (options.method.toLowerCase() === 'get') {
+
+    if (options.data) {
+      let dataRes = Object.entries(options.data)
+
+      dataRes.forEach((element, index) => {
+        if (index === 0) {
+          urlSource = urlSource + '?' + element.join('=')
+
+        } else {
+          urlSource = urlSource + "&" + element.join('=')
+        }
+      });
+    }
+  } else {
+    if (options.data) {
+      for (var key in data) {
+        formData.append(key, data[key])
+      }
+    }
+  }
 
   try {
-    let getDataOption = () => {
-      let dataUrlOptions = []
-
-      if (Object.keys(options.data).length > 1) {
-        for (let i of Object.entries(options.data)) {
-          dataUrlOptions.push(i[0] + '=' + i[1])
-        }
-      } else {
-        for (let i of Object.entries(options.data)) {
-          dataUrlOptions.push(i[0] + '=' + i[1])
-        }
+    xhr.open(options.method, urlSource)
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', () => {
+      if (Boolean(options.callback)) {
+        options.callback(xhr.onerror, xhr.response)
       }
-      dataUrlOptions = dataUrlOptions.join('&')
+    })
 
-      return dataUrlOptions
-    }
-
-    let postDataOptions = () => {
-      for (let i of Object.entries(options.data)) {
-        formData.append(i[0], i[1])
-      }
-    }
-
-    if (options.method === 'GET' && Boolean(options.data)) {
-      xhr.open(options.method, `${options.url}${getDataOption()}`)
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', () => {
-        if (Boolean(options.callback)) {
-          options.callback(xhr.onerror, xhr.response)
-        }
-      })
+    if (options.method.toLowerCase() === 'get') {
       xhr.send()
-    } else if (options.method === 'GET' && !Boolean(options.data)) {
-      xhr.open(options.method, `${options.url}`)
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', () => {
-        if (Boolean(options.callback)) {
-          options.callback(xhr.onerror, xhr.response)
-        }
-      })
-      xhr.send()
-    }
-
-    if (options.method === 'POST') {
-      postDataOptions()
-
-      xhr.open(options.method, options.url)
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', () => {
-        if (Boolean(options.callback)) {
-          options.callback(xhr.onerror, xhr.response)
-        }
-      })
+    } else {
       xhr.send(formData)
     }
 
-    if (options.method === 'PUT') {
-      postDataOptions()
-
-      xhr.open(options.method, options.url)
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', () => {
-        if (Boolean(options.callback)) {
-          options.callback(xhr.onerror, xhr.response)
-        }
-      })
-      xhr.send(formData)
-    }
-
-    if (options.method === 'DELETE') {
-      postDataOptions()
-
-      xhr.open(options.method, options.url)
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', () => {
-        if (Boolean(options.callback)) {
-          options.callback(xhr.onerror, xhr.response)
-        }
-      })
-      xhr.send(formData)
-    }
   } catch (e) {
     callback(e);
   }
--- public/js/ui/pages/TransactionsPage.js
+++ public/js/ui/pages/TransactionsPage.js
@@ -10,7 +10,7 @@ class TransactionsPage {
    * Сохраняет переданный элемент и регистрирует события
    * через registerEvents()
    * */
-  constructor( element ) {
+  constructor(element) {
     if (!element) {
       throw new Error('элемента не существует');
     };
@@ -36,14 +36,6 @@ class TransactionsPage {
     this.element.addEventListener('click', e => {
       let removeAccount = this.element.querySelector('.remove-account');
       let transactionRemove = this.element.querySelector('.transaction__remove');
-
-      // removeAccount.addEventListener('click', e => {
-      //   TransactionsPage.removeAccount()
-      // })
-
-      // transactionRemove.addEventListener('click', e => {
-      //   TransactionsPage.removeTransaction()
-      // })
     })
   }
 
@@ -66,7 +58,7 @@ class TransactionsPage {
    * По удалению транзакции вызовите метод App.update(),
    * либо обновляйте текущую страницу (метод update) и виджет со счетами
    * */
-  removeTransaction( id ) {
+  removeTransaction(id) {
 
   }
 
@@ -76,20 +68,19 @@ class TransactionsPage {
    * Получает список Transaction.list и полученные данные передаёт
    * в TransactionsPage.renderTransactions()
    * */
-  render(options){
+  render(options) {
     if (!options) return
     this.lastOptions = options;
 
     Account.get(this.lastOptions.account_id, (err, response) => {
-    if (response.success) {
-      this.renderTitle(response.data.name)
-
-      Transaction.list(response.data, (err, response) => {
-        console.log(response)
-      })
+      if (response.success) {
+        this.renderTitle(response.data.name)
+        Transaction.list(response.data, (err, response) => {
+          console.log(response)
+        })
       };
-    })  
-}
+    })
+  }
 
   /**
    * Очищает страницу. Вызывает
@@ -103,7 +94,7 @@ class TransactionsPage {
   /**
    * Устанавливает заголовок в элемент .content-title
    * */
-  renderTitle(name){
+  renderTitle(name) {
     this.element.querySelector('.content-title').textContent = name
   }
 
@@ -111,7 +102,7 @@ class TransactionsPage {
    * Форматирует дату в формате 2019-03-10 03:20:41 (строка)
    * в формат «10 марта 2019 г. в 03:20»
    * */
-  formatDate(date){
+  formatDate(date) {
 
   }
 
@@ -119,7 +110,7 @@ class TransactionsPage {
    * Формирует HTML-код транзакции (дохода или расхода).
    * item - объект с информацией о транзакции
    * */
-  getTransactionHTML(item){
+  getTransactionHTML(item) {
 
   }
 
@@ -127,7 +118,7 @@ class TransactionsPage {
    * Отрисовывает список транзакций на странице
    * используя getTransactionHTML
    * */
-  renderTransactions(data){
+  renderTransactions(data) {
 
   }
 }
